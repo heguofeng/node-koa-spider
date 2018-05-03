514196f635828b87d3a590584c039db21d2dce60
heguofeng <32927202@qq.com>
2018-05-04 00:35:21 +0800

    新增打印时间格式，目前一次性拉取整个网站数据，稍微降低了一开始时间间隔。 有个问题是 时不时node会停止爬取，但没有退出。目目前设置了下载时timeout大于30s就当错误处理。

--- async.js
+++ async.js
@@ -7,6 +7,10 @@ const async = require('async')
 const proxy = "http://61.135.217.7:80";
 
 var headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        'Connection': 'keep-alive',
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36",
         "Host": "www.mmjpg.com"
     }
@@ -15,23 +19,26 @@ var urls = []; //图片地址标题库
 var arrAjax = []; //ajax api数组
 var imgSrcs = []; //api里的图片链接数组
 function getcount() {
-    superagent.get("http://img.mmjpg.com/2018/1248/1.jpg")
-        .set({ 'Host': 'img.mmjpg.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36' })
-        .set("Referer", "http://www.mmjpg.com/home/16")
+    superagent.get("http://www.mmjpg.com/")
+        .set(headers)
+        .set("Referer", "http://www.mmjpg.com/mm/1338")
+        // .timeout(1000)
         .end((err, res) => {
             if (err) {
                 console.log(err)
-
             } else if (res === undefined) {
                 return
             } else if (res.statusCode == 200) {
-                console.log(res.headers['content-length'])
-                    // var $ = cheerio.load(res.text);
-                    // let pageCount = parseInt($('.main #page #opic').prev().text().trim());
-                    // for (i = 0; i <= pageCount; i++) {
-                    //     console.log(i)
-                    // }
+                console.log(res)
 
+                // var $ = cheerio.load(res.text);
+                // // let pageCount = parseInt($('.main #page #opic').prev().text().trim());
+                // // for (i = 0; i <= pageCount; i++) {
+                // //     console.log(i)
+                // // }
+                // //http://img.mmjpg.com/2017/1200/1.jpg
+                // let imgurl = $("#content a img").attr("src").match(/^http\:\/\/img\.mmjpg\.com\/201\d{1}/);
+                // console.log(imgurl)
             }
 
         })
--- index.js
+++ index.js
@@ -9,20 +9,27 @@ let baseUrl = "http://www.mmjpg.com/";
 let urlArr = []; //包含每个人的具体信息的数组
 let proxy = "http://61.135.217.7:80";
 var headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    'Connection': 'keep-alive',
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36",
     "Host": "www.mmjpg.com"
 }
-
-
-
-
-
+var headers_img = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    'Connection': 'keep-alive',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36",
+    "Host": "img.mmjpg.com"
+};
 //一开始获取每个人的具体信息
 async function getUrl() {
-    console.log("获取所有的home页面集合");
+    console.log(formatDateTime(new Date()) + "获取所有的home页面集合");
     let webUrlArr = [];
     let url;
-    for (let i = 1; i < 20; i++) {
+    for (let i = 1; i < 90; i++) {
         if (i == 1) {
             url = "http://www.mmjpg.com/";
             webUrlArr.push(url); //整个网站所有的页面
@@ -36,18 +43,18 @@ async function getUrl() {
     return new Promise((resolve, reject) => {
         let q = async.queue((webUrl, callback) => {
             // console.log("开始获取每个home页面每个人的具体信息")
-            var delay = parseInt((Math.random() * 30000000) % 1000 + 1000, 10); //设置延时并发爬取
+            var delay = parseInt((Math.random() * 30000000) % 1000 + 200, 10); //设置延时并发爬取
             concurrencyCount++;
-            console.log('现在的并发数是', concurrencyCount, '，正在获取的是', webUrl, '延迟', delay, '毫秒')
+
+            console.log(formatDateTime(new Date()) + '现在的并发数是', concurrencyCount, '，正在获取的是', webUrl, '延迟', delay, '毫秒')
             superagent.get(webUrl)
                 .set(headers)
-                .set("Referer", "http://www.mmjpg.com/home/11")
+                .set("Referer", "http://www.mmjpg.com/home/12")
                 // .proxy(proxy)
                 .end((err, res) => {
                     if (err) {
-                        console.log('访问页面出错啦' + webUrl)
+                        console.log(formatDateTime(new Date()) + '访问页面出错啦' + webUrl)
                         console.log(err);
-
                         concurrencyCount--;
                         callback(null)
                     } else if (res === undefined) {
@@ -55,7 +62,7 @@ async function getUrl() {
                         callback(null)
                         return
                     } else if (res.statusCode == 200) {
-                        console.log("访问成功", webUrl)
+                        console.log(formatDateTime(new Date()) + "访问成功", webUrl)
                         var $ = cheerio.load(res.text);
                         var elem = $('.main .pic ul li')
                         for (let i = 0; i < elem.length; i++) {
@@ -76,7 +83,7 @@ async function getUrl() {
                 })
         }, 5);
         q.drain = function() {
-            console.log("获取结束,这里包含每个人的具体信息的数组", urlArr)
+            console.log(formatDateTime(new Date()) + "获取结束,这里包含每个人的具体信息的数组", urlArr)
             resolve(urlArr)
         }
         q.push(webUrlArr)
@@ -85,15 +92,15 @@ async function getUrl() {
 
 //根据图片链接获取每个系列的所有图片src
 async function getAllSrcs() {
-    console.log('获取每个系列的所有图片src，开始执行。。。')
+    console.log(formatDateTime(new Date()) + '获取每个系列的所有图片src，开始执行。。。')
     var concurrencyCount = 0;
     var urlArr = await getUrl(); //进行第一步
     let allImgUrls = [];
     return new Promise((resolve, reject) => {
         let q = async.queue((urlArr, callback) => {
-            var delay = parseInt((Math.random() * 30000000) % 1000 + 1000, 10); //设置延时并发爬取
+            var delay = parseInt((Math.random() * 30000000) % 1000 + 500, 10); //设置延时并发爬取
             concurrencyCount++;
-            console.log('现在的并发数是', concurrencyCount, '，正在获取的是', urlArr.title, '延迟', delay, '毫秒')
+            console.log(formatDateTime(new Date()) + '现在的并发数是', concurrencyCount, '，正在获取的是', urlArr.title, '延迟', delay, '毫秒')
                 //判断是否需要用ajax的拼接
             if (urlArr.id >= 1256) {
                 superagent
@@ -103,7 +110,8 @@ async function getAllSrcs() {
                     // .proxy(proxy)
                     .end((err, res) => {
                         if (err) {
-                            console.log(err)
+                            console.log(err);
+                            concurrencyCount--;
                             callback(null)
                         } else if (res === undefined) {
                             concurrencyCount--;
@@ -111,10 +119,10 @@ async function getAllSrcs() {
                         } else if (res.statusCode == 200) {
                             var dir = urlArr.title;
                             if (fs.existsSync(path.join(__dirname, '/pic', dir))) {
-                                console.log("已存在该文件名")
+                                console.log(formatDateTime(new Date()) + "已存在该文件名")
                             } else {
                                 fs.mkdir(path.join(__dirname, '/pic', dir))
-                                console.log(`成功创建文件夹 ${dir}`)
+                                console.log(formatDateTime(new Date()) + `成功创建文件夹 ${dir}`)
                             }
 
                             var arr = res.text.split(","); //获取后缀
@@ -151,17 +159,18 @@ async function getAllSrcs() {
                         } else if (res.statusCode == 200) {
                             var dir = urlArr.title;
                             if (fs.existsSync(path.join(__dirname, '/pic', dir))) {
-                                console.log("已存在该文件名")
+                                console.log(formatDateTime(new Date()) + "<----已存在该文件名--->" + dir)
                             } else {
                                 fs.mkdir(path.join(__dirname, '/pic', dir))
-                                console.log(`成功创建文件夹 ${dir}`)
+                                console.log(formatDateTime(new Date()) + `<----成功创建文件夹----> ${dir}`)
                             }
 
                             var $ = cheerio.load(res.text);
                             let pageCount = parseInt($('.main #page #opic').prev().text().trim());
+                            let _imgSrc = $("#content a img").attr("src").match(/^http\:\/\/img\.mmjpg\.com\/\d{4}\//); //前缀
                             for (let i = 1; i <= pageCount; i++) {
                                 let item = {}
-                                item.imgSrc = "http://img.mmjpg.com/2018/" + urlArr.id + "/" + i + '.jpg';
+                                item.imgSrc = _imgSrc + urlArr.id + "/" + i + '.jpg';
                                 item.title = dir;
                                 item.id = urlArr.id;
 
@@ -178,7 +187,7 @@ async function getAllSrcs() {
 
         }, 5)
         q.drain = function() {
-            console.log("获取了每个人的所有图片地址 已完成")
+            console.log(formatDateTime(new Date()) + "获取了每个人的所有图片地址 已完成")
             resolve(allImgUrls)
             console.log(allImgUrls)
         }
@@ -187,40 +196,41 @@ async function getAllSrcs() {
 }
 
 let downloadImg = async function() {
-    console.log("开始下载图片...");
+    let startTime = new Date().getTime(); //开始时间
+    console.log(formatDateTime(new Date()) + "开始下载图片...");
     let errDownloadCount = 0;
     let downloadCount = 0;
     let concurrencyCount = 0;
     let q = async.queue(function(image, callback) {
         const filename = image.imgSrc.split('/').pop()
         if (fs.existsSync(path.join(__dirname, 'pic', image.title, filename))) {
-            console.log("已存在该文件，略过")
+            console.log("已存在该文件，略过---->", image.imgSrc)
             callback(null);
         } else {
             let delay = parseInt((Math.random() * 30000000) % 1000 + 1000, 10);
             concurrencyCount++;
-            console.log(`现在的并发数是${concurrencyCount}，正在下载${image.title}的${filename}，延迟${delay}mm`);
+            console.log(`现在的并发数是${concurrencyCount}，正在下载${image.title}的${image.imgSrc}，延迟${delay}mm`);
             return new Promise((resolve, reject) => {
                 try {
                     superagent.get(image.imgSrc)
-                        .set({ 'Host': 'img.mmjpg.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36' })
+                        .set(headers_img)
                         .set('Referer', 'http://www.mmjpg.com/mm/' + image.id)
+                        .timeout(30000)
                         .end((err, res) => {
                             try {
                                 if (err) {
-                                    console.log(filename + "下载失败");
+                                    console.log(formatDateTime(new Date()) + image.imgSrc + "---->下载失败");
                                     errDownloadCount++;
-                                    console.log(err);
                                     concurrencyCount--;
                                     callback(null)
                                 } else if (res === undefined) {
-                                    console.log(filename + "内容为undefined，下载失败");
+                                    console.log(formatDateTime(new Date()) + filename + "---->内容为undefined，下载失败");
                                     errDownloadCount++;
                                     concurrencyCount--;
                                     callback(null)
                                 } else if (res.statusCode == 200) {
-                                    if (res.headers['content-length'] < 20000) { //如果图片太小，直接取消下载
-                                        console.log(filename + "该图片太小，取消下载")
+                                    if (parseInt(res.headers['content-length']) <= 20000) { //如果图片太小，直接取消下载
+                                        console.log(formatDateTime(new Date()) + image.imgSrc + "---->该图片太小，取消下载")
                                         concurrencyCount--;
                                         callback(null)
                                     } else {
@@ -230,7 +240,7 @@ let downloadImg = async function() {
                                                 console.log(err);
                                                 callback(null)
                                             }
-                                            console.log(filename + "下载成功");
+                                            console.log(formatDateTime(new Date()) + filename + "<----下载成功---->", "图片大小为" + (res.headers['content-length'] / 1000) + "KB");
 
                                         });
                                         setTimeout(() => {
@@ -240,14 +250,14 @@ let downloadImg = async function() {
                                     }
                                 }
                             } catch (error) {
-                                console("里面哪里出错啦", error);
+                                console.log(formatDateTime(new Date()) + "里面哪里出错啦", error);
                                 concurrencyCount--;
                                 callback(null, filename)
                             }
 
                         });
                 } catch (error) {
-                    console("哪里出错啦", error);
+                    console.log(formatDateTime(new Date()) + "哪里出错啦", error);
                     concurrencyCount--;
                     callback(null, filename)
                 }
@@ -258,7 +268,9 @@ let downloadImg = async function() {
 
     // 当所有任务都执行完以后，将调用该函数
     q.drain = function() {
-        console.log('All img download,一共下载了' + downloadCount + "张图片;", "下载失败", errDownloadCount, "张");
+        console.log(formatDateTime(new Date()) + 'All img download,一共下载了' + downloadCount + "张图片;", "下载失败", errDownloadCount, "张");
+        execTime = new Date().getTime() - startTime;
+        console.log("一共耗时" + execTime + 'mm')
     }
     let images = await getAllSrcs();
     q.push(images)
@@ -266,3 +278,18 @@ let downloadImg = async function() {
 
 
 downloadImg()
+    //定义时间格式
+function formatDateTime(date) {
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    var second = date.getSeconds();
+    second = second < 10 ? ('0' + second) : second;
+    return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second + " ";
+};
